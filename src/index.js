25e9fac4104530e9b90e8668be64911f59705d40
Bad-Raider <stanislav.gusiev@gmail.com>
2023-04-27 17:33:53 +0300

    fix
    1/ added h1 tag
    2/ added more css style
    3/ deleted commite

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import fetchCountries from './js/fetchCountries';
-// import { deleteMarkup, alertInfo, alertWrong, creatorFullMarkup, creatorMarkup } from './js/functions';
 import markupCountryList from './templates/markupCountryList.hbs';
 import markupCountryInfo from './templates/markupCountryInfo.hbs';
 
@@ -51,21 +50,16 @@ function deleteMarkup() {
   refs.contryInfoEl.innerHTML = "";
   refs.countryListEl.innerHTML = "";
 };
-
 function alertInfo() {
   Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
 };
-
 function alertWrong() {
   Notiflix.Notify.failure("Oops, there is no country with that name.")
 };
-
 function creatorFullMarkup(countyObj) {
   refs.countryListEl.insertAdjacentHTML("beforeend", markupCountryList(countyObj));
-        refs.contryInfoEl.insertAdjacentHTML("beforeend", markupCountryInfo(countyObj));
+  refs.contryInfoEl.insertAdjacentHTML("beforeend", markupCountryInfo(countyObj));
 };
-
 function creatorMarkup(countyObj) {
-          refs.countryListEl.insertAdjacentHTML("beforeend", markupCountryList(countyObj));
-
+  refs.countryListEl.insertAdjacentHTML("beforeend", markupCountryList(countyObj));
 };
